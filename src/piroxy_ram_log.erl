39588f5cd8c5a357a886b16b12df873049e6d16a
Justin Davis <jrcd83@gmail.com>
2020-12-07 09:52:40 -0500

    Fix piroxy_ram_log and add calls to query ETS tables.

--- src/piroxy_ram_log.erl
+++ src/piroxy_ram_log.erl
@@ -2,11 +2,12 @@
 -behavior(gen_event).
 -include("../include/phttp.hrl").
 -include_lib("kernel/include/logger.hrl").
--import(lists, [foreach/2]).
+-import(lists, [foreach/2, map/2, zip/2, unzip/1, reverse/1]).
 -record(state, {matchfun=nomatch, conntab, logtab, bodytab,
-                accum=dict:new()}).
+                accum=#{}}).
 
 -export([start_link/0, watchman/0]).
+-export([filter/1, connections/0, log/1, body/1]).
 -export([init/1, handle_event/2, handle_call/2]).
 
 %%%
@@ -28,6 +29,18 @@ watchman() ->
             end
     end.
 
+filter(Term) ->
+    gen_event:call(piroxy_events, ?MODULE, {filter,Term}).
+
+connections() ->
+    gen_event:call(piroxy_events, ?MODULE, connections).
+
+log(ConnId) ->
+    gen_event:call(piroxy_events, ?MODULE, {log,ConnId}).
+
+body(Digest) ->
+    gen_event:call(piroxy_events, ?MODULE, {body,Digest}).
+
 %%%
 %%% BEHAVIOR CALLBACKS
 %%%
@@ -40,6 +53,9 @@ init([]) ->
 handle_event(_, #state{matchfun=nomatch}=S) ->
     {ok, S};
 
+handle_event(T, #state{matchfun=matchall}=S) ->
+    log_event(T, S);
+
 handle_event(T, S) ->
     MatchFun = S#state.matchfun,
     case MatchFun(T) of
@@ -49,8 +65,28 @@ handle_event(T, S) ->
             {ok,S}
     end.
 
-handle_call(_, S) ->
-    {ok,ok,S}.
+handle_call({filter,A}, S) when is_atom(A) ->
+    if
+        A == matchall; A == nomatch ->
+            {ok, ok, S#state{matchfun=A}};
+        true ->
+            {ok, {error,badarg}, S}
+    end;
+
+handle_call(connections, S) ->
+    {ok, ets:tab2list(S#state.conntab), S};
+
+handle_call({log,ConnId}, S) ->
+    {ok, ets:lookup(S#state.logtab, ConnId), S};
+
+handle_call({body,Digest}, S) ->
+    Body = case ets:lookup(S#state.bodytab, Digest) of
+               [] ->
+                   not_found;
+               [Bin] ->
+                   Bin
+           end,
+    {ok,Body,S}.
 
 %%%
 %%% HIDDEN
@@ -65,22 +101,27 @@ log_event({Id,Dir,Time,http,Term}=T, S)
     case Term of
         #head{bodylen=0} ->
             %% body will not be accumulated
+            io:format("*DBG* [~B:~s] empty body no accum~n", [Id,Dir]),
             ets:insert(S#state.logtab, T),
             {ok, S};
         #head{}=Head ->
+            io:format("*DBG* [~B:~s] non-empty body~n", [Id,Dir]),
             ets:insert(S#state.logtab, T),
             M0 = S#state.accum,
             M = M0#{{Id,Dir} => {Head,[]}},
             {ok, S#state{accum=M}};
         {body,Body} ->
             {ok, accum_body(Id, Dir, Time, Body, S)};
+        {fail,Reason}
+          when Reason == cancelled; Reason == reset ->
+            {ok, store_body(Id, Dir, S)};
         eof ->
             {ok, store_body(Id, Dir, S)}
     end;
 
 log_event(T, S) ->
     ets:insert(S#state.logtab, T),
-    S.
+    {ok,S}.
 
 accum_body(Id, Dir, Time, Bin, S) ->
     K = {Id,Dir},
@@ -90,22 +131,19 @@ accum_body(Id, Dir, Time, Bin, S) ->
 
 store_body(Id, Dir, S) ->
     case S#state.accum of
-        #{{Id,Dir} := {_Head,L0}} ->
-            L = lists:reverse(L0),
-            Body = iolist_to_binary([X || {_,X} <- L]),
-            Digest = crypto:hash(sha512, Body),
+        #{{Id,Dir} := {_Head,L1}} ->
+            {Stamps,Chunks} = unzip(reverse(L1)),
+            Digest = crypto:hash(sha512, Chunks),
             LogTab = S#state.logtab,
             BodyTab = S#state.bodytab,
-            Fun = fun ({Time,Bin}) ->
-                          %% XXX: do {body,...} messages always use binary?
-                          T = {Id,Time,Dir,http,{body,Digest,byte_size(Bin)}},
-                          ets:insert(LogTab, T)
-                  end,
-            foreach(Fun, Body),
-            ets:insert(BodyTab, {Digest,Body}),
+            L2 = zip(Stamps, map(fun erlang:iolist_size/1, Chunks)),
+            foreach(fun({Tstamp,Size}) ->
+                            T = {Id,Tstamp,Dir,http,{body,Digest,Size}},
+                            ets:insert(LogTab, T)
+                    end, L2),
+            ets:insert(BodyTab, {Digest,Chunks}),
             S#state{accum=maps:remove({Id,Dir}, S#state.accum)};
         _ ->
-            ?LOG_ERROR("could not finalize body for unknown Id: ~p", [Id]),
-            %% XXX: delete other records for Id?
+            %% there were no body messages
             S
     end.
